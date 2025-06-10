31309994a55f00e3fce8e4982fcedaf6197f0ef9
Gavin Wentzel <gavinwentzel@gmail.com>
2025-06-10 14:46:10 -0400

    Added view portfolio button to home screen

--- Artfolio/src/components/hero/Hero.jsx
+++ Artfolio/src/components/hero/Hero.jsx
@@ -1,38 +1,50 @@
 import React from 'react'
 import hero from '../../assets/keys1.jpeg'
 import electricButterfield from '../../assets/electricButterfield.jpeg'
+import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Hero() {
   return (
-    <section id='home' className='relative flex items-center justify-center overflow-hidden min-h-[780px] sm:min-h-[600px]' >
-        <div className='flex flex-col md:flex-row md:space-x-4 font-arima px-8 max-w-5xl mx-auto md:mt-32 mt-[-180px]'>
-            <div data-aos='fade-up' data-aos-delay='500' className='flex-1 space-y-6 transform'>
-                <p className='text-sm text-gray-600 '>Blending Genres and Mediums </p>
-                <h1 className='text-5xl font-semibold text-gray-900'>Visual & Musical Artist</h1>
-                <div className='flex items-center'>
-                    <span className='w-8 h-8 bg-orange-300 rounded-full'></span>
-                    <span className='w-8 h-8 bg-blue-300 rounded-full'></span>
-                    <button className='-ml-5 font-bold text-black rounded-full z-10'></button>
-                </div>
-            </div>
-            <div data-aos='fade-up' data-aos-delay='600' className='flex-1 relative'>
-                <div className='grid grid-cols-1 gap-4'>
-                    <div className='relative z-10 md:right-[-200px] right-[-150px] md:bottom-40 bottom-[-20px]'>
-                        <img src={electricButterfield} alt="Image 1" className='rounded-lg md:w-40 md:h-40 w-28 h-28 object-cover' />
-                    </div>
-
-                   
-                    <div className='absolute md:bottom-20 md:left-[-40px] md:w-80 md:h-80 w-52 h-52 p-2 rounded-lg '>
-                        <img src={hero} alt="Image" className='rounded-md'/>
-                    </div>
-                    <div className='absolute md:bottom-[-20px] bottom-[-180px] md:right-[-20px] right-[8px] transform rotate-[-30deg]'>
-                        <p className='font-serif text-sm text-gray-800 tracking-wider'>
-                           
-                        </p>
-                    </div>
-                </div>
-            </div>
+    <section
+      id="home"
+      className="relative flex items-center justify-center overflow-hidden min-h-[780px] sm:min-h-[600px]"
+    >
+      <div className="flex flex-col md:flex-row md:space-x-4 font-arima px-8 max-w-5xl mx-auto md:mt-32 mt-[-180px]">
+        <div data-aos="fade-up" data-aos-delay="500" className="flex-1 space-y-6 transform">
+          <p className="text-sm text-gray-600">Blending Genres and Mediums</p>
+          <h1 className="text-5xl font-semibold text-gray-900">Visual & Musical Artist</h1>
+          <div className="flex items-center space-x-2">
+            <span className="w-8 h-8 bg-orange-300 rounded-full"></span>
+            <a
+                    href="#products"
+                    className="mt-8 inline-block px-6 py-3 bg-green-700 hover:bg-green-800 text-white rounded-lg transition"
+                    >
+                    View Portfolio
+                </a>
+            <span className="w-8 h-8 bg-blue-300 rounded-full"></span>
+           
+          </div>
+          
         </div>
+
+        <div data-aos="fade-up" data-aos-delay="600" className="flex-1 relative">
+          <div className="grid grid-cols-1 gap-4">
+            <div className="relative z-10 md:right-[-200px] right-[-150px] md:bottom-40 bottom-[-20px]">
+              <img
+                src={electricButterfield}
+                alt="Electric Butterfield"
+                className="rounded-lg md:w-40 md:h-40 w-28 h-28 object-cover"
+              />
+            </div>
+
+            <div className="absolute md:bottom-20 md:left-[-40px] md:w-80 md:h-80 w-52 h-52 p-2 rounded-lg">
+              <img src={hero} alt="Artist Hero" className="rounded-md w-full h-full object-cover" />
+            </div>
+
+          </div>
+        
+        </div>
+      </div>
     </section>
   )
 }
